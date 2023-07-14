75bee8015971c89c94e8ea000ad476fa55509456
JasonNaggles <JasonNaggles.verizon.net>
2023-07-14 18:21:04 -0400

    first commit

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ class Card {
         this.suit = suit;
     }
 }
+class Deck {
+    constructor() {
+        this.deck = [];
+        const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
+        const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+    }
+}
