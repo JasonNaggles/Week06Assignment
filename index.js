611f9bcbe5da4de3584a5d68bce7fc24b16f738e
JasonNaggles <JasonNaggles.verizon.net>
2023-07-14 18:40:50 -0400

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,28 @@ class Card {
 class Deck {
     constructor() {
         this.deck = [];
+
         const suits = ['Spades', 'Hearts', 'Clubs', 'Diamonds'];
         const ranks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+    
+
+
+    for (let i = 0; i < suits.length; i++) {
+        for (let j = 0; j < ranks.length; j++) {
+            this.deck.push(new Card(ranks[j], j = 2, suits[i]));
+     }
     }
+
+}
+
+
+shuffle() {
+    for (let i = 0; i < suits.length; i--) {
+        let randomIndex = Math.floor(Math.random() * (i + 1));
+        let currentIndex = this.deck[i];
+        this.deck[i] = this.deck[randomIndex];
+        this.deck[randomIndex] = currentIndex;
+    }
+    return this.deck;
+}
 }
