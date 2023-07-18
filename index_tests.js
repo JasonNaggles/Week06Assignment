b98ffd2d48851e4dba6540ca518a3559acd7d99b
JasonNaggles <JasonNaggles.verizon.net>
2023-07-18 00:03:37 -0400

    first commit

--- index_tests.js
+++ index_tests.js
@@ -1,5 +1,7 @@
-const assert = require("chai").assert;
-const { Deck } = require("../index.js");
+const expect = chai.expect;
+
+
+
 
 
 describe('MyFunctions', function() {
@@ -7,14 +9,14 @@ describe('MyFunctions', function() {
         it('Should return an array of 52 cards', function() {
         var deck = new Deck;
         let shuffle = deck.shuffle();
-        assert.equal(shuffle.length, 52);
+        expect.equal(shuffle.length, 52);
 });
 
 it('Should return an array that is not in the same order as the original', function() {
     var controlDeck = new Deck;
     var shuffleDeck = new Deck;
     shuffle = shuffleDeck.shuffle();
-    assert.notDeepEqual(shuffle, controlDeck.deck);
+    expect.notEqual(shuffle, controlDeck.deck);
 });
 
     });
