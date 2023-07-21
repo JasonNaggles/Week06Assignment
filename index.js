d53701796b53cc8e8b2591215af96eebe7dbb013
JasonNaggles <JasonNaggles.verizon.net>
2023-07-21 18:30:39 -0400

    first commit

--- index.js
+++ index.js
@@ -1,39 +1,3 @@
-class Players {
-    constructor(points) {
-        this.playerOneTotalPoints = 0;
-        this.playerTwoTotalPoints = 0;
-        this.ties = 0;
-    }
-    compareCards(valuePlayerOne, valuePlayerTwo) {
-        if (valuePlayerOne.value === valuePlayerTwo.value) {
-            players.addPoints('nobobyWon')
-            return 'nobodyWon'
-        } else if (valuePlayerOne.value > valuePlayerTwo.value) {
-            players.addPoints('playerOne')
-            return 'playerOne'
-        } else {
-            players.addPoints('playerTwo')
-            return 'playerTwo'
-        }
-    }
-    addPoints(player) {
-        if (player == 'nobodyWon') {
-            this.ties++;
-        } else if (player == 'playerOne') {
-            this.playerOneTotalPoints++;
-        } else {
-            this.playerTwoTotalPoints++;
-        }
-    }
-
-}
-class Card {
-    constructor(rank, value, suit) {
-        this.rank = rank;
-        this.value = value;
-        this.suit = suit;
-    }
-}
 class Deck {
     constructor() {
         this.cardDeck = [];
@@ -73,15 +37,46 @@ class Deck {
 
 
 
-shuffle() {
-    for (let i = this.suits.length; i > 0; i--) {
+shuffleDeck() {
+    for (let i = this.cardDeck.length; i > 0; i--) {
         let randomIndex = Math.floor(Math.random() * (i + 1));
-        let currentIndex = this.deck[i];
-        this.deck[i] = this.deck[randomIndex];
-        this.deck[randomIndex] = currentIndex;
+        let currentIndex = this.cardDeck[i];
+        this.cardDeck[i] = this.cardDeck[randomIndex];
+        this.cardDeck[randomIndex] = currentIndex;
     }
     return this.deck;
 }
+
+}
+class Players {
+    constructor(points) {
+        this.playerOneTotalPoints = 0;
+        this.playerTwoTotalPoints = 0;
+        this.ties = 0;
+    }
+    compareCards(valuePlayerOne, valuePlayerTwo) {
+        if (valuePlayerOne.value === valuePlayerTwo.value) {
+            players.addPoints('nobobyWon')
+            return 'nobodyWon'
+        } else if (valuePlayerOne.value > valuePlayerTwo.value) {
+            players.addPoints('playerOne')
+            return 'playerOne'
+        } else {
+            players.addPoints('playerTwo')
+            return 'playerTwo'
+        }
+    }
+    addPoints(player) {
+        if (player == 'nobodyWon') {
+            this.ties++;
+        } else if (player == 'playerOne') {
+            this.playerOneTotalPoints++;
+        } else {
+            this.playerTwoTotalPoints++;
+        }
+    }
+
+
 playGame() {
     while(deck.cardDeck.length > 0);
     let one = deck.cardDeck.pop();
