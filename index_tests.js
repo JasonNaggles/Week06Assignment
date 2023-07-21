cbed21087554d70e3ecef29d19ea56c58febb35a
JasonNaggles <JasonNaggles.verizon.net>
2023-07-21 19:04:17 -0400

    first commit

--- index_tests.js
+++ index_tests.js
@@ -4,7 +4,7 @@ let expect = chai.expect;
 describe('createDeck()', () => {
     describe('test the deck', () => {
         it('Should return an array of 52 cards', () => {
-        expect(deck).to.have.lengthOf(52);
+        expect(cardDeck).to.have.lengthOf(52);
 });
 
 it('Should return an array that is not in the same order as the original', () => {
